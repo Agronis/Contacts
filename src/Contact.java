097596fa4938093cc59bf7da35c3f77baf24466c
Agronis <agronis@icloud.com>
2015-10-13 10:21:52 -0400

    for loop examples w/ data structures.

---
+++ src/Contact.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Agronis on 10/13/15.
+ */
+public class Contact {
+
+    public String name;
+    public String address;
+    public int age;
+
+    public Contact(String name, int age) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+}
